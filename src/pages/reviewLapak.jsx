bcefe6966cf20d38f0c587b35617c78eb4dca5f6
Nathaniel Vincent <nathanielvincentnv@gmail.com>
2024-10-10 10:16:56 +0700

    bikin page review-lapak

---
+++ src/pages/reviewLapak.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReviewLapak, Navbar } from "../components/index";  
+
+function Review() {
+  return (
+    <>
+        <ReviewLapak />
+        <Navbar />
+    </>
+  );
+}
+
+export default Review;
