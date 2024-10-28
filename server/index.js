03bbbd1841f18ab013c1e7658b239b1f5193a029
Nathaniel Vincent <nathanielvincentnv@gmail.com>
2024-10-28 19:28:17 +0700

    commit last

--- server/index.js
+++ server/index.js
@@ -334,7 +334,7 @@ app.get('/api/test-db', (req, res) => {
   });
   
   
-app.get('/api/lapak', (req, res) => {
+  app.get('/api/lapak', (req, res) => {
     const currentDay = new Date().getDay();
     
     const query = `
@@ -453,7 +453,7 @@ app.get('/api/lapak', (req, res) => {
             return res.json({ 
                 success: true, 
                 lapaks,
-                message: ${lapaks.length} lapak ditemukan
+                message: `${lapaks.length} lapak ditemukan`
             });
 
         } catch (error) {
